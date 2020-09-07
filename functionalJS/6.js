657a28af6f500293ade837e996cdd1905fc4284c
Krishna <7krishna7dhakal7@gmail.com>
2020-09-07 11:32:48 +0545

    reduce reduced me

---
+++ functionalJS/6.js
@@ -0,0 +1,10 @@
+function countWords(inputWords) {
+    // SOLUTION GOES HERE
+    return inputWords.reduce((total,currentValue,currentIndex,arr)=>{
+        
+        
+    })
+  }
+  
+  module.exports = countWords
+
---
+++ functionalJS/6_1.js
@@ -0,0 +1,12 @@
+function countWords(inputWords) {
+    // SOLUTION GOES HERE
+    return inputWords.reduce(({},currentValue,currentIndex,arr)=>{
+       
+            {}[currentValue]=count;
+                
+    })
+  }
+  
+countWords(['a','b','b','c','a']);
+
+
