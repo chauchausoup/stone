a36e160f9cb3e760684638688320b99d2109a1de
Krishna <7krishna7dhakal7@gmail.com>
2020-09-10 16:31:36 +0545

    computed property

--- tower_of_babel/6.js
+++ tower_of_babel/6.js
@@ -1,2 +1,32 @@
 //computed property in objects
 
+//before
+/* var prop ="foo";
+var obj ={
+    [(()=>"foo" + "molly")()]:"bar"
+
+};
+
+console.log(obj)
+
+*/
+
+
+var obj = {
+    [(()=>{
+        if(process.argv[2]%2===0){
+            return "even"
+        }else{
+            return "odd"
+        }
+    })()]:process.argv[2],
+    [process.argv[2]+process.argv[3]]:process.argv[2]+process.argv[3]
+}
+
+console.log(obj)
+
+
+
+
+
+
